32184b2625431b90ae81f860a7996570b5629eb1
Rebecca Weiss <rebecca.j.weiss@gmail.com>
2011-11-23 21:35:07 -0800

    testing fork

--- nytimes_nlp.py
+++ nytimes_nlp.py
@@ -4,11 +4,24 @@ import os
 import cPickle as pickle
 
 class Article(object):
-	def __init__(self, headline, lead, body):
-		self.headline = headline
-		self.lead = lead
-		self.body = body
-	def 
+
+	#might not need access to all these attributes, but for now might as well define them
+	def __init__(self, article_dict):
+		self.headline = article_dict['headline']
+		self.lead = article_dict['lead']
+		self.body = article_dict['body']
+		self.pagenum = article_dict['pagenum']
+		self.taxclass = article_dict['taxclass']
+		self.descriptors = article_dict['descriptors']
+		self.pagecol = article_dict['pagecol']
+		self.month = article_dict['month']
+		self.pagesect = article_dict['pagesect']
+		self.year = article_dict['year']
+		self.date = article_dict['date']
+		self.general_desc = article_dict['general_desc']
+		self.day = article_dict['day']
+
+	
 
 def main():
 	main_path = '/home/rebecca/Desktop/final project/'
