c2daabd3cf01c6320dc6f1974177d6b3420ac79b
Rebecca Weiss <rebecca.j.weiss@gmail.com>
2011-11-24 16:36:12 -0800

    getting ngrams from articles

--- nytimes_nlp.py
+++ nytimes_nlp.py
@@ -1,39 +1,62 @@
-import nltk
 import re
 import os
 import cPickle as pickle
+import nltk
+import nltk.data
+from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktWordTokenizer
+from nltk import bigrams, trigrams
+
 
 class Article(object):
 
 	#might not need access to all these attributes, but for now might as well define them
 	def __init__(self, article_dict):
-		self.headline = article_dict['headline']
-		self.lead = article_dict['lead']
-		self.body = article_dict['body']
-		self.pagenum = article_dict['pagenum']
-		self.taxclass = article_dict['taxclass']
-		self.descriptors = article_dict['descriptors']
-		self.pagecol = article_dict['pagecol']
-		self.month = article_dict['month']
-		self.pagesect = article_dict['pagesect']
-		self.year = article_dict['year']
-		self.date = article_dict['date']
-		self.general_desc = article_dict['general_desc']
-		self.day = article_dict['day']
+		#TODO: go back to nytimes_parser.py and include .xml filename in dictionary
+		self.data = article_dict
 
-	
+	def preprocess(self):
+		#using the punkt tokenizer because it is trained on real data
+		sentence_tokenizer = nltk.data.load('nltk:tokenizers/punkt/english.pickle')
+		word_tokenizer = PunktWordTokenizer()
+		
+		#title
+		self.headline_words = word_tokenizer.tokenize(self.data['headline'])
+		self.headline_bigrams = bigrams(self.headline_words)
+		self.headline_trigrams = trigrams(self.headline_words)
 
+		#lead
+		self.lead_sentences = sentence_tokenizer.tokenize(self.data['lead'], realign_boundaries=True)
+		self.lead_words = [word_tokenizer.tokenize(sentence) for sentence in self.lead_sentences]
+		self.lead_bigrams = bigrams(self.lead_words)
+		self.lead_trigrams = trigrams(self.lead_words)
+
+		#body
+		self.body_sentences = sentence_tokenizer.tokenize(self.data['body'], realign_boundaries=True)
+		self.body_words = [word_tokenizer.tokenize(sentence) for sentence in self.body_sentences]
+		self.body_bigrams = bigrams(self.body_words)
+		self.body_trigrams = trigrams(self.body_words)
+
+		#TODO: remove caps, punctuation?
+
+#	def tagging(self):
+		
 def main():
 	main_path = '/home/rebecca/Desktop/final project/'
 	os.chdir(main_path)
 
-	nytimes = open('nytimes2000.pkl', 'r')
-	nytimes = pickle.load(nytimes)
+	articles_pickle = open('nytimes2000.pkl', 'r')
+	articles_pickle = pickle.load(articles_pickle)
 
 	articles = []
-	for article in nytimes:
-		articles.append(Article(article['headline'], article['lead'], article['body']))	
+	for article in articles_pickle:
+		articles.append(Article(article))
 
+	for article in articles:
+		article.preprocess()
+
+	articles_pickle = open('new_nytimes2000.pkl', 'w')
+	pickle.dump(articles, articles_pickle)
+	
 if __name__ == '__main__':
 	main()
 	print "Done."
