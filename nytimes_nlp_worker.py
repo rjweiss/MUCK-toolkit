d7f523df42bab51e03435fd9abdae76d8a0365c9
Rebecca Weiss <rebecca.j.weiss@gmail.com>
2011-11-27 00:25:35 -0800

    changed pickle process for multiprocessing

--- nytimes_nlp_worker.py
+++ nytimes_nlp_worker.py
@@ -9,11 +9,6 @@ from nltk import bigrams, trigrams
 import itertools
 import re
 
-#class Article(object):
-
-#	def __init__(self, article_dict):
-#		self.data = article_dict
-
 def get_tokens(dict):
 	#using the punkt tokenizer because it is trained on real data
 	sentence_tokenizer = nltk.data.load('nltk:tokenizers/punkt/english.pickle')
@@ -58,28 +53,6 @@ def main():
 	if file.endswith('.pkl'):
 		print "Converting " + file
 		convert_dir(file)
-	#main_path = '/home/rebecca/Desktop/fp/'
-	#convert_dir(main_path + '2000/')
-
-	#articles = []
-	#for article in articles_pickle:
-	#	articles.append(Article(article))
-
-	#count = 0
-	#for article in articles:
-	#	count += 1
-	#	article.tokenizing()
-	#	article.ngrams()
-	#	if (count % 1000 == 0):
-	#		print 'Processed %d files.' % count
-
-	#there is an error here with ipython; because i am pickling in the __main__ namespace, Article will not be in the fake.__dict__ namespace, so you have to manually set it equal to Article when loading the pickle file...super hacky
-	#fake=__import__('__main__')
-	#fake.__dict['Article'] = Article
-
-	#os.chdir(main_path + '2000/pickle')
-	#articles_pickle = open('processed_nytimes2000.pkl', 'wb')
-	#pickle.dump(articles, articles_pickle)
 	
 if __name__ == '__main__':
 	main()
