1984a17f3dcd05b599296893fc835878e7658ebf
Rebecca Weiss <rjweiss@stanford.edu>
2012-04-28 22:35:02 -0700

    Connected to local mongodb, walked through the mongodb tutorial with the articles db.

--- src/edu/stanford/rjweiss/newspaper/NewspaperProject.java
+++ src/edu/stanford/rjweiss/newspaper/NewspaperProject.java
@@ -7,5 +7,32 @@ package edu.stanford.rjweiss.newspaper;
  * Time: 6:12 PM
  * To change this template use File | Settings | File Templates.
  */
+
 public class NewspaperProject {
+    public static void main(String[] args) throws Exception {
+//        Mongo m = new Mongo();
+//        DB db = m.getDB("test");
+//        DBCollection articles = db.getCollection("articles");
+//        DBObject myDoc = articles.findOne();
+//        DBCursor cur;
+//        BasicDBObject query = new BasicDBObject();
+
+//        query.put("file", "66276940.xml");
+//        cur = articles.find(query);
+//
+//        while (cur.hasNext()) {
+//            System.out.println(cur.next());
+//        }
+
+
+//        int n = 0;
+//
+//        while (cur.hasNext() && n++ < 100) {
+//            System.out.println(cur.next());
+//        }
+//        System.out.println(myDoc);
+//        System.out.println(articles.getCount());
+
+
+    }
 }
