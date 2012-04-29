e3dd9625f5d8146550daa6ca450a0306cb5b1331
Rebecca Weiss <rjweiss@stanford.edu>
2012-04-28 20:19:08 -0700

    Testing intellij integration.

---
+++ src/edu/stanford/rjweiss/newspaper/NewspaperProject.java
@@ -0,0 +1,11 @@
+package edu.stanford.rjweiss.newspaper;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Rebecca
+ * Date: 4/28/12
+ * Time: 6:12 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NewspaperProject {
+}
