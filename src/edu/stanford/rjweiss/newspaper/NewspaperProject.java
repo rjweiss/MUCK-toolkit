a33afd939730e7c69100816b4bb72a02a9dbf613
Rebecca Weiss <rjweiss@stanford.edu>
2012-04-29 13:11:25 -0700

    Retrieved all articles from 2010.

--- src/edu/stanford/rjweiss/newspaper/NewspaperProject.java
+++ src/edu/stanford/rjweiss/newspaper/NewspaperProject.java
@@ -1,5 +1,11 @@
 package edu.stanford.rjweiss.newspaper;
 
+import com.mongodb.*;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Rebecca
@@ -10,29 +16,38 @@ package edu.stanford.rjweiss.newspaper;
 
 public class NewspaperProject {
     public static void main(String[] args) throws Exception {
-//        Mongo m = new Mongo();
-//        DB db = m.getDB("test");
-//        DBCollection articles = db.getCollection("articles");
-//        DBObject myDoc = articles.findOne();
-//        DBCursor cur;
-//        BasicDBObject query = new BasicDBObject();
+        Mongo m = new Mongo();
+        DB db = m.getDB("test");
+        DBCollection articles = db.getCollection("articles");
+        DBObject myDoc = articles.findOne();
+        DBCursor cur;
+        BasicDBObject query = new BasicDBObject();
 
-//        query.put("file", "66276940.xml");
-//        cur = articles.find(query);
-//
-//        while (cur.hasNext()) {
-//            System.out.println(cur.next());
-//        }
-
-
+//      some tutorial exploration
 //        int n = 0;
-//
+
 //        while (cur.hasNext() && n++ < 100) {
 //            System.out.println(cur.next());
 //        }
 //        System.out.println(myDoc);
 //        System.out.println(articles.getCount());
 
+
+//        retrieve a single document
+//        query.put("file", "66276940.xml"); //returns a single document, the string is the name of the .xml file
+//        cur = articles.find(query); // will retrieve the document in the articles collection that matches the .xml query
+
+//        this section will print out the document retrieved
+//        while (cur.hasNext()) {
+//            DBObject o = cur.next();
+//            System.out.println(o);
+//        }
+
+        Calendar cal = new GregorianCalendar();
+        cal.set(2010, 0, 1); // retrieving from January 1st, 2010
+        Date fromDate = cal.getTime();
+        query.put("date", new BasicDBObject("$gte", fromDate));
+        System.out.println(articles.count(query)); //returns 157134 total articles
 
     }
 }
